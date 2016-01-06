23adc8a565c1997a6fa21310c9c84dc697f19a2d
Mário Patrão <mariopatrao@gmail.com>
2016-01-06 12:09:53 +0000

    v1.0

---
+++ API/Controllers/ModelAController.cs
@@ -0,0 +1,56 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace API.Controllers
+{
+    public class ModelAController : ApiController
+    {
+
+        // I only use the Post method because this allow me with the use of [FromBody] to use a list of variables instead of the URI string,
+        // this values are passed by the .net Model in this case ModelA.
+        // Using the GET method we could only pass a string as an argument and in addition we would more likely needed to use [FromURI] 
+
+        // POST api
+        public HttpResponseMessage Post([FromBody] ModelA Values)
+        {
+            try
+            {
+
+                string Command = Values.Command;
+                string varA = Values.varA;
+                string varB = Values.varB;
+
+
+                // Insert Logic here related to the command passed
+                // EX:  GET DB VALUES
+                //      GetSomething()
+                // 
+                //      INSERT INTO db,
+                //      Insert(varA)
+                //
+                //      DELETE FROM DB
+                //      Delete(varB)
+
+
+                // Using the Request.CreateResponse allows to pass more arguments ex: Json with a list of users. 
+                return Request.CreateResponse(HttpStatusCode.OK, "You might want to pass more data like a json");
+
+                //return new HttpResponseMessage(HttpStatusCode.OK);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ API/Models/ModelA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class ModelA
+    {
+        // The Model should contain all the variables used for the commands in this API Model (ModelA)
+        // Command will exist in all Models (ModelA, ModelB, etc) and will be the variable that will decide which function to use (Ex: Insert user)
+        public string Command { get; set; }
+        public string varA { get; set; }
+        public string varB { get; set; }
+    }
+}
